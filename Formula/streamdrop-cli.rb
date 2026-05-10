83aa4890e5997c8ea9d579731323206ed68caa9f
Antony Leons <antony@leons.dev>
2026-05-10 03:03:06 +0100

    feat(packaging): add chocolatey package and update homebrew formula
    - Add Chocolatey package definition and install script template
    - Update Homebrew formula with detailed installation blocks and test
    - Improve Scoop manifest structure and add checkver field
    - Remove old Homebrew formula template file

--- Formula/streamdrop-cli.rb
+++ Formula/streamdrop-cli.rb
@@ -1,15 +1,24 @@
 class StreamdropCli < Formula
-  desc "Secure E2EE, zero storage streaming transfer CLI"
-  homepage "https://github.com/AntonyLeons/streamdrop"
+  desc "End-to-end encrypted, zero-storage file transfer CLI"
+  homepage "https://streamdrop.app"
   version "${VERSION}"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/AntonyLeons/streamdrop/releases/download/v${VERSION}/streamdrop-cli-macos-x64"
       sha256 "${MAC_INTEL_SHA}"
-    elsif Hardware::CPU.arm?
+
+      def install
+        bin.install "streamdrop-cli-macos-x64" => "streamdrop"
+      end
+    end
+    if Hardware::CPU.arm?
       url "https://github.com/AntonyLeons/streamdrop/releases/download/v${VERSION}/streamdrop-cli-macos-arm64"
       sha256 "${MAC_ARM_SHA}"
+
+      def install
+        bin.install "streamdrop-cli-macos-arm64" => "streamdrop"
+      end
     end
   end
 
@@ -17,10 +26,14 @@ class StreamdropCli < Formula
     if Hardware::CPU.intel?
       url "https://github.com/AntonyLeons/streamdrop/releases/download/v${VERSION}/streamdrop-cli-linux-x64"
       sha256 "${LINUX_INTEL_SHA}"
+
+      def install
+        bin.install "streamdrop-cli-linux-x64" => "streamdrop"
+      end
     end
   end
 
-  def install
-    bin.install Dir["streamdrop-cli-*"].first => "streamdrop"
+  test do
+    system "#{bin}/streamdrop", "--help"
   end
 end
