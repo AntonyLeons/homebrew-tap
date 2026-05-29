02b2b5523f1e429153bbd52b4d1a0158b5f3c578
Antony Leons <antony@leons.dev>
2026-05-29 02:34:31 +0100

    bump version to 1.0.16

--- Formula/streamdrop-cli.rb
+++ Formula/streamdrop-cli.rb
@@ -1,22 +1,22 @@
 class StreamdropCli < Formula
   desc "End-to-end encrypted, zero-storage file transfer CLI"
   homepage "https://streamdrop.app"
-  version "1.0.15"
+  version "1.0.16"
 
   on_macos do
     on_intel do
-      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.15/streamdrop-cli-macos-x64"
+      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.16/streamdrop-cli-macos-x64"
       sha256 "ac96c375c622f3e4fe196b2a676be9cc3707fa58fcf3dc005fedfe24e705ab1b"
     end
     on_arm do
-      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.15/streamdrop-cli-macos-arm64"
+      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.16/streamdrop-cli-macos-arm64"
       sha256 "6e54c9a0a74ff7eae1101535725cefcbf681f7342063b1fe0bab25491d511f7d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.15/streamdrop-cli-linux-x64"
+      url "https://github.com/AntonyLeons/streamdrop/releases/download/v1.0.16/streamdrop-cli-linux-x64"
       sha256 "be0296cc79149d4321a3677c65395401c3925bebd51209a8aa26a1602b0a8dea"
     end
   end
